db7d628ecb34b8128bb68cfc56b3b9d272c4cb01
Kors557 <1500459@mail.ru>
2022-08-31 02:46:54 +0300

    recipes 1.1

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -1,3 +1,90 @@
+from django.contrib.auth import get_user_model
+from django.core.validators import MinValueValidator
 from django.db import models
 
-# Create your models here.
+User = get_user_model()
+
+
+class Ingredient(models.Model):
+    name = models.CharField(
+        verbose_name='Название ингредиента',
+        max_length=200
+    )
+    measurement_unit = models.CharField(
+        verbose_name='Еденица измерения',
+        max_length=200
+    )
+
+    class Meta:
+        ordering = ('name', )
+        verbose_name = 'Ингредиент'
+        verbose_name_plural = 'Ингредиенты'
+
+
+class Tag(models.Model):
+    name = models.CharField(
+        verbose_name='Тэг',
+        max_length=200,
+        unique=True
+    )
+    color = models.CharField(
+        verbose_name='Цвет в HEX',
+        max_length=7,
+        unique=True
+    )
+    slug = models.SlugField(
+        verbose_name='Уникальный слаг',
+        max_length=200,
+        unique=True
+    )
+
+    class Meta:
+        ordering = ('id',)
+        verbose_name = 'Тег'
+        verbose_name_plural = 'Теги'
+
+
+class Recipe(models.Model):
+    tags = models.ManyToManyField(
+        Tag,
+        verbose_name='Тег'
+    )
+    author = models.ForeignKey(
+        User,
+        verbose_name='Автор',
+        on_delete=models.CASCADE,
+        related_name='recipes'
+    )
+    ingredients = models.ManyToManyField(
+        Ingredient,
+        verbose_name='Ингредиенты'
+    )
+    name = models.CharField(
+        verbose_name='Название',
+        max_length=200
+    )
+    image = models.ImageField(
+        verbose_name='Картинка',
+        upload_to='recipes/images/'
+    )
+    text = models.TextField(
+        verbose_name='Описание'
+    )
+    cooking_time = models.PositiveIntegerField(
+        verbose_name='Время готовки',
+        validators=MinValueValidator(
+            1,
+            message='Время не может бытб меньше 1 мин'
+        )
+    )
+    pub_date = models.DateField(
+        verbose_name='Дата публикации',
+        auto_now_add=True
+    )
+
+    class Meta:
+        ordering = ('-pub_date')
+        verbose_name = 'Рецепт'
+        verbose_name_plural = 'Рецепты'
+
+
