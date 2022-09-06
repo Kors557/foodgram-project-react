12b56f304e8ea50e758a7982beb9f46ac0e8dc03
Kors557 <1500459@mail.ru>
2022-09-06 21:59:39 +0300

    urls

--- backend/foodgram/urls.py
+++ backend/foodgram/urls.py
@@ -4,11 +4,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('api/auth/', include('djoser.urls.authtoken')),
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
