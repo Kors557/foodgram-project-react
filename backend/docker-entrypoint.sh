bd05686a11789fa3a277f83680b5e2d7e8ce7340
Kors557 <1500459@mail.ru>
2022-09-07 00:08:04 +0300

    server-2.1

---
+++ backend/docker-entrypoint.sh
@@ -0,0 +1,8 @@
+echo "Start migrations..."
+python manage.py migrate
+
+echo "Collect static..."
+python manage.py collectstatic --no-input
+
+echo "Start foodgram..."
+gunicorn foodgram.wsgi:application --bind 0.0.0.0:8000 
