cfe22291a1cb0971a83ba96db4b55cb6cdd96d61
Kors557 <1500459@mail.ru>
2022-09-06 20:52:07 +0300

    server-1-4

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -12,7 +12,7 @@ SECRET_KEY = os.getenv(
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['158.160.7.128', '127.0.0.1', 'localhost',]
+ALLOWED_HOSTS = ['158.160.7.128', '127.0.0.1', 'localhost']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
